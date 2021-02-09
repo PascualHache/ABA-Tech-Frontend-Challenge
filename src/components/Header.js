76959729a2452ab0a9b6a8e9c4464589454ffc5c
Hache <hector.pascual.diaz@gmail.com>
2021-02-09 16:07:28 +0100

    Header first aproach

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <>
+        <h1 className="title-heading">Your favourite tunes</h1>;
+        <h2 className="subtitle-heading">All ☀️ and all 🌜</h2>
+    </>
+};
+export default Header;
