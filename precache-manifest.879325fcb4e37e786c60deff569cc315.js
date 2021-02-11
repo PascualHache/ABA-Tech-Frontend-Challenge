07c69a4c8f0e426b993b6edd185214b8ab1cf880
Hache <hector.pascual.diaz@gmail.com>
2021-02-11 22:12:15 +0100

    Updates

---
+++ precache-manifest.879325fcb4e37e786c60deff569cc315.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f5a7558f94fe7ae7eab527a49c511f",
+    "url": "/ABA-Tech-Frontend-Challenge/index.html"
+  },
+  {
+    "revision": "0f91db280d058553f003",
+    "url": "/ABA-Tech-Frontend-Challenge/static/css/main.304177f1.chunk.css"
+  },
+  {
+    "revision": "1b46d4b0bb3987c281dc",
+    "url": "/ABA-Tech-Frontend-Challenge/static/js/2.a367362e.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/ABA-Tech-Frontend-Challenge/static/js/2.a367362e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f91db280d058553f003",
+    "url": "/ABA-Tech-Frontend-Challenge/static/js/main.1fb5ca13.chunk.js"
+  },
+  {
+    "revision": "26112868fb3f46c35e80",
+    "url": "/ABA-Tech-Frontend-Challenge/static/js/runtime-main.0677043d.js"
+  },
+  {
+    "revision": "8d5ea91351989c394b92ea05d5019c82",
+    "url": "/ABA-Tech-Frontend-Challenge/static/media/avatar.8d5ea913.svg"
+  },
+  {
+    "revision": "ee1d2f309079138dcfdbd52521b250fe",
+    "url": "/ABA-Tech-Frontend-Challenge/static/media/hero.ee1d2f30.svg"
+  }
+]);
