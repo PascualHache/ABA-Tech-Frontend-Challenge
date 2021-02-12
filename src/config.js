43c8b453529670786ec09c6bb72be43ac66e10a6
Hache <hector.pascual.diaz@gmail.com>
2021-02-12 12:24:10 +0100

    gh-pages

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/authorize',
     clientSecret: '',
-    redirectUrl: 'http://PascualHache.github.io/ABA-Tech-Frontend-Challenge/redirect',
+    redirectUrl: 'https://PascualHache.github.io/ABA-Tech-Frontend-Challenge/redirect',
     newReleaseUrl: 'https://api.spotify.com/v1/browse/new-releases',
     featuredPlaylistsUrl: 'https://api.spotify.com/v1/browse/featured-playlists',
     categoriesUrl: 'https://api.spotify.com/v1/browse/categories'
